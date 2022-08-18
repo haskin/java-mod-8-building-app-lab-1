8c7b02ea9e5f1913db713dac634528e3918a24df
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-18 11:35:30 -0400

    finished lab 1 implementation

--- messenger-app/src/app/app.module.ts
+++ messenger-app/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConversationHistoryComponentComponent } from './component/conversation-history-component/conversation-history-component.component';
+import { ConversationThreadComponentComponent } from './component/conversation-thread-component/conversation-thread-component.component';
+import { ContactListComponentComponent } from './component/contact-list-component/contact-list-component.component';
+import { ConversationControlComponentComponent } from './component/conversation-control-component/conversation-control-component.component';
+import { SendMessageComponentComponent } from './component/send-message-component/send-message-component.component';
+import { SenderMessageComponentComponent } from './component/sender-message-component/sender-message-component.component';
+import { UserMessageComponentComponent } from './component/user-message-component/user-message-component.component';
+import { ContactComponentComponent } from './component/contact-component/contact-component.component';
+import { HeaderComponentComponent } from './component/header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversationHistoryComponentComponent,
+    ConversationThreadComponentComponent,
+    ContactListComponentComponent,
+    ConversationControlComponentComponent,
+    SendMessageComponentComponent,
+    SenderMessageComponentComponent,
+    UserMessageComponentComponent,
+    ContactComponentComponent,
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule
