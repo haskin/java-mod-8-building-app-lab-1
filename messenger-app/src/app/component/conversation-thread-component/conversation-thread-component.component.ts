ca31b9fe0e6f72b299134f0657874ae0cdfb83cf
Adrian Khaskin <haskin.dev@gmail.com>
2022-08-18 12:12:53 -0400

    finsihed lab 3

--- messenger-app/src/app/component/conversation-thread-component/conversation-thread-component.component.ts
+++ messenger-app/src/app/component/conversation-thread-component/conversation-thread-component.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConversationThreadComponentComponent implements OnInit {
 
+  senderMessages = [
+    {
+      sender: { firstName: "Ludovic" },
+      text: "Message from Ludovic",
+      conversationId: 1,
+      sequenceNumber: 0,
+    },
+    {
+      sender: { firstName: "Jessica" },
+      text: "Message from Jessica",
+      conversationId: 1,
+      sequenceNumber: 1,
+    },
+  ];
+
+  userMessages = [
+    {
+      sender: { firstName: "Aurelie" },
+      text: "Message from Aurelie",
+      conversationId: 1,
+      sequenceNumber: 2,
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
